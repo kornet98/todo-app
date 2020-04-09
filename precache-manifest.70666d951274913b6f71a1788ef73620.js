cda08997290426f6f77bb69cfdbf0f91efa59c8b
Volodymyr_Danylko <danylko91@gmail.com>
2020-04-10 00:07:36 +0300

    Updates

---
+++ precache-manifest.70666d951274913b6f71a1788ef73620.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec6a9c92be7463a8e4abd460966c563",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "cfa6e09f550ef3219327",
+    "url": "/todo-app/static/css/main.70e57751.chunk.css"
+  },
+  {
+    "revision": "24700fffb8de51aec8bc",
+    "url": "/todo-app/static/js/2.2df9a5ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.2df9a5ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfa6e09f550ef3219327",
+    "url": "/todo-app/static/js/main.fd2152e7.chunk.js"
+  },
+  {
+    "revision": "8c574afb4aca7e7fd22e",
+    "url": "/todo-app/static/js/runtime-main.e86c861d.js"
+  }
+]);
